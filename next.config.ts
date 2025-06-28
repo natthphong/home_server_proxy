16eaaa5e73696d35b73b93a157134f75cd8f37f2
Natthaphong Jaroenpronprasit <113831963+natthphong@users.noreply.github.com>
2025-06-28 21:23:16 +0700

    feat: add proxy rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    const base = process.env.NEXT_PUBLIC_EXTERNAL_API_BASE_URL || "";
+    return [
+      {
+        source: "/:service/api/v1/:path*",
+        destination: `${base}/:service/api/v1/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
